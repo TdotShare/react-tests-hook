1e21685b75d3be90563fb9fde80bc627aabb0adc
Jirayu Ch <52649379+TdotShare@users.noreply.github.com>
2022-02-04 15:32:31 +0700

    check useEffect
    check useEffect componentDidUpdate

--- src/example01/home.js
+++ src/example01/home.js
@@ -1,7 +1,47 @@
-import React from 'react';
+import React , { useState , useEffect } from 'react';
 
-function home() {
-  return <div>test !</div>;
+
+
+function Home() {
+
+  const [count, setCount] = useState(0)
+  const [item , setItem] = useState(0)
+
+
+
+
+  // useEffect(() => { // componentDidMount
+  //   console.log(`rerender home component ! (componentDidMount)`)
+  // }, []);
+
+
+  // useEffect(() => { // componentDidUpdate
+  //   console.log(`rerender home component ! (componentDidUpdate)`)
+  // }); // No second argument, so run after every render.
+
+  // useEffect(() => { // componentDidUpdate
+  //   console.log(`item rerender`)
+  // } , [item]); // จะไม่ rerender หาก item ไม่มีการเปลี่ยนแปลง
+
+
+
+
+  console.log(`rerender home component !`)
+
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click me
+      </button>
+
+      <br/>
+
+      <button onClick={() => setItem(item + 1)}>
+        Click Item {item}
+      </button>
+    </div>
+  );
 }
 
-export default home;
+export default Home;
